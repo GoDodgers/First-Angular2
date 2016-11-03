98b865b75147aad80c27e9d6f4381b55fa227959
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-11-02 19:21:40 -0700

    [comments] adds inline template and style comments to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,31 @@ import { Component } from '@angular/core';
 	selector: 'app-root', //possible: '#app-root' === <div id="app-root"> || '.app-root' === <div class="app-root">
 	//required, EACH COMPONENT needs 1, NOT ZERO, NOT TWO, and ONLY 1, template
 	templateUrl: './app.component.html',
+
+	/*
+
+	template literals can be added using the template property, ex:
+	template: `
+		<h1>Inline Template</h1>
+	`,
+	
+	*/
+
 	//optional, supports multiple styles, component does not need styling
 	styleUrls: [
 		'./app.component.css'
 	]
+
+	/*
+
+	like templates inline styles can be added using the styles property
+	styles: [`
+		h1 {
+			color: red;
+		}
+	`]
+
+	*/
 })
 
 export class AppComponent {
