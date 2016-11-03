3b6a8250da126ef98dd2193e7df7586c1a5e5f5e
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-11-02 18:49:37 -0700

    [comments] adds comments to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	//required, EACH COMPONENT needs 1, NOT ZERO, NOT TWO, and ONLY 1, template
+	templateUrl: './app.component.html',
+	//optional, supports multiple styles, component does not need styling
+	styleUrls: [
+		'./app.component.css'
+	]
 })
+
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
 }
