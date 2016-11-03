07c2c6049ca26851b736528aeba2aa68244b3b17
Alberto M Esquivias <alberto.m.esquivias@gmail.com>
2016-11-02 19:13:03 -0700

    [comments] adds comments to app.module.ts, index.html, main.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,29 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
+	//declares which directives/pipes we use in our application
+	//directives are like instructions that tell angular 2 what to do
 	declarations: [
+		//componets are such instructions
 		AppComponent
 	],
+	//shows angular2 which other modules do we use in our application
 	imports: [
+		//default angular2 modules
 		BrowserModule,
 		FormsModule,
 		HttpModule
+		//end default modules
 	],
+	//application wide services
 	providers: [
 	],
+	//component that starts our angular2 applicationsent to main.ts
 	bootstrap: [
 		AppComponent
 	]
 })
+
+//NOTE: empty class AppModule gets exported
+//recieves all its properties from the @NgModule decorator which is imported from angular/core
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+//file that is run first
+
 import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -5,8 +7,13 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 
+//checks development mode which dictates some of the debug messages
 if (environment.production) {
 	enableProdMode();
 }
 
+//this line bootstraps, starts, angular2 application
+//.bootstraoModule() method takes one argument, and this the module we want to start the application
+//by default angular2 does not load all components/selectors and if multiple components are avaiable this is 
+//how angular2 knows what compnent to load first from app.module.ts file as bootstrap prop
 platformBrowserDynamic().bootstrapModule(AppModule);
